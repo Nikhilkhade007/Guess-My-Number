7aef90888fa489147c0e1d476ae08b99c0b5770c
Nikhil Khade <128150274+Nikhilkhade007@users.noreply.github.com>
2023-03-27 11:32:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 'use strict';
 
-let number = Math.trunc(Math.random() * 20) + 1;
+let secretnumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 const x = function () {
   const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No Number!';
-  }
-  if (score > 0) {
-    if (guess === number) {
+    document.querySelector('.message').textContent = '🛑 Put some number!';
+  } else if (score > 0) {
+    if (guess === secretnumber) {
       document.querySelector('.message').textContent = '🎉 Correct Number!';
       if (score > highscore) highscore = score;
-      document.querySelector('.number').textContent = number;
+      document.querySelector('.number').textContent = secretnumber;
       document.querySelector('.highscore').textContent = highscore;
       document.body.style.backgroundColor = '#60b347';
       document.querySelector('.number').style.width = '30rem';
-    } else if (guess > number) {
+    } else if (guess > secretnumber) {
       document.querySelector('.message').textContent = '📈 Too high';
       score--;
       document.querySelector('.score').textContent = score;
@@ -33,7 +32,7 @@ const x = function () {
 };
 document.querySelector('.check').addEventListener('click', x);
 let again = function () {
-  number = Math.trunc(Math.random() * 20) + 1;
+  secretnumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('.number').textContent = '?';
   score = 20;
   document.querySelector('.score').textContent = 20;
